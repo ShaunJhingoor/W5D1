9fe0cf35f58032b7c1873ed41e0d89e3402c3da6
Shaun Jhingoor <Jhingoor1945@gmail.com>
2023-11-13 16:19:44 -0500

    start po2

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,12 +4,25 @@ end
 
 class Array
   def hash
-    my_arr = self.map {|ele| ele = ele.hash}
-  end
+    self.inject{|acc,el| acc + el * 2}.hash
+  #  total = 0
+  #  self.each do |ele|
+  #   total += ele.hash
+  # end
+  # total
+
+end
 end
 
 class String
   def hash
+   arr = self.chars
+   total = []
+   arr.each_with_index do |el, i|
+    total << i
+   end 
+
+   total.inject{|acc,el| acc + el * 2}.hash
   end
 end
 
